371363d0e52f59eeb85523ebd4cfe200cc54db17
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2021-04-22 15:18:08 -0500

    updated error messages

--- src/app/config/errors.ts
+++ src/app/config/errors.ts
@@ -1,7 +1,7 @@
 export enum ERROR_MESSAGES {
   CLEAR = '',
-  OUT_OF_VOTES = 'You are out of votes this month.',
-  DUPLICATE_VOTE = 'You have already voted for that.',
-  ERROR_GETTING_SNACKS = 'There was an error getting the snacks. Please try again another time.',
-  ERROR_CASTING_VOTE = 'There was an error casting your vote. Please try again another time.',
+  OUT_OF_VOTES = "You are out of votes this month. Don't forget to vote again next month!",
+  DUPLICATE_VOTE = 'You have already voted for that; pick something else.',
+  ERROR_GETTING_SNACKS = 'There was an error getting the snacks. Please, try again another time.',
+  ERROR_CASTING_VOTE = 'There was an error casting your vote. Please, try again.',
 }
